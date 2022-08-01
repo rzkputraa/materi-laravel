e40f833980a5e2b10b00cf243af8b95716521bc5
Rizki Putra Wicaksono <rizkiputra1100@gmail.com>
2022-08-01 15:42:40 +0700

    materi controller dan model

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,7 @@ Route::post('/form-post', function (Request $request) {
 Route::get('/registrasi', function () {
     return view();
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
